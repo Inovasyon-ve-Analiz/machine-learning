019e84a251420f5d6b167b5ec5379e7f8151de98
yerminal <yerminal@users.noreply.github.com>
2021-02-15 20:01:23 +0300

    Update abdullah.py

--- 1. Hafta/abdullah.py
+++ 1. Hafta/abdullah.py
@@ -12,16 +12,15 @@ from sklearn import linear_model
 
 dataY=pd.read_csv('covid_19_data_tr.csv', sep=',',header=None)[1:][2].to_numpy(
     ).reshape(-1,1).astype('float64')
-dataX = np.arange(1,dataY.size+1).reshape(-1,1)
-dataX_next = np.arange(dataX.size,dataX.size+76).reshape(-1,1)
+dataX = np.arange(1,41+76).reshape(-1,1) # 41 training examples
 
 regr = linear_model.LinearRegression()
-regr.fit(dataX, dataY)
-dataY_pred = regr.predict(np.concatenate((dataX, dataX_next)))
+regr.fit(dataX[:41], dataY)
+dataY_pred = regr.predict(dataX)
 
 print("The confirmed cases are %d after 75 days." %dataY_pred[-1])
-plt.scatter(dataX, dataY,  color='black')
-plt.plot(np.concatenate((dataX, dataX_next), axis=0), dataY_pred, color='blue', linewidth=3)
+plt.scatter(dataX[:41], dataY,  color='black')
+plt.plot(dataX, dataY_pred, color='blue', linewidth=3)
 plt.xticks(())
 plt.yticks(())
 plt.show()
