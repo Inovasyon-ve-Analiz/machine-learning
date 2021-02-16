24edf06db8345f46be937d62573da0209ef35c27
ienesb <63236773+ienesb@users.noreply.github.com>
2021-02-16 18:02:48 +0300

    Update img_to_vector.py

--- 3. Hafta/img_to_vector.py
+++ 3. Hafta/img_to_vector.py
@@ -1,4 +1,4 @@
 import cv2
 
 def img_to_vector(file_name,w,h):
-    return cv2.resize(cv2.imread(file_name,0),(w,w)).reshape(1,w*h)[0]
+    return cv2.resize(cv2.imread(file_name,0),(w,h)).reshape(1,w*h)[0]
