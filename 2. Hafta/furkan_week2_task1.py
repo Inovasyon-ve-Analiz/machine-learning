ebb1e66d42086fb5d60be6751e91207ca7195573
gencf <79080187+gencf@users.noreply.github.com>
2021-02-17 13:55:12 +0300

    Update furkan_week2_task1.py

--- 2. Hafta/furkan_week2_task1.py
+++ 2. Hafta/furkan_week2_task1.py
@@ -5,16 +5,23 @@ from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import Ridge
+from sklearn import preprocessing
 
 
 db = pd.read_csv("covid_19_data_tr.csv")
 x = np.arange(1,42).reshape(-1, 1)
 y = np.array(db["Confirmed"]).reshape(-1, 1)
 
+"""
+scaler = preprocessing.StandardScaler().fit(x)
+x = scaler.transform(x)
+print(x)
+"""
+
 # Ridge() can also be used as linear regression model
 degree = 3
 model = make_pipeline(PolynomialFeatures(degree), linear_model.LinearRegression())
-model.fit(x,y)
+model.fit(x, y)
 prediction = int(model.predict(np.array([75]).reshape(1, -1)).ravel())
 print("The predicted number of covid-19 case at 75th day:",prediction)
 
