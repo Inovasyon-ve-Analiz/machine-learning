ffc67c70df7e04437573ea6bfbfa9d5e46035c8c
ienesb <ienes9625@gmail.com>
2021-02-17 14:12:52 +0300

    Update ismail2.py

--- 2. Hafta/ismail2.py
+++ 2. Hafta/ismail2.py
@@ -7,14 +7,11 @@ from sklearn.model_selection import train_test_split
 
 data = pd.read_csv("car_fuel_consumption.csv" ,thousands='.', decimal=',')
 data = data[data["gas_type"] =="E10"]
-distance = data["distance"].astype("float64")
-speed = data["speed"].astype("float64")
-temp_outside = data["temp_outside"].astype("float64")
+x = np.array(data[["distance", "speed", "temp_outside"]],dtype=np.float64)
 consume = data["consume"].astype("float64").to_numpy()
 
-x = np.vstack((distance,speed,temp_outside)).T
 
-x_train, x_test, y_train, y_test = train_test_split(x,consume,test_size=0.75,shuffle=True)
+x_train, x_test, y_train, y_test = train_test_split(x,consume,test_size=0.25,shuffle=True)
 
 model = linear_model.LinearRegression()
 model.fit(x_train, y_train)
@@ -22,3 +19,4 @@ prediction = model.predict(x_test)
 score = model.score(x_test,y_test)
 print(y_test)
 print(prediction)
+print(score*100)
