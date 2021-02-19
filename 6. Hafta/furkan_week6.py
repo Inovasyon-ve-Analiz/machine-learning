ada27f953c271bf3cc89c79d66dcd8bade494c13
gencf <79080187+gencf@users.noreply.github.com>
2021-02-19 09:16:05 +0300

    furkan

--- 6. Hafta/furkan_week6.py
+++ 6. Hafta/furkan_week6.py
@@ -6,18 +6,21 @@ import glob
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import learning_curve
+from sklearn.model_selection import ShuffleSplit
+
 
 a = 100
 drones = np.ndarray((1,a**2))
 for filename in glob.glob('../dataset/drone/*.jpg'):
     img = cv2.resize(cv2.imread(filename,0),(a,a)).reshape(1,a**2)[0]
-    drones = np.vstack((drones,img))  
+    drones = np.vstack((drones,img))
 drones = drones[1:]
 
 antis = np.ndarray((1,a**2))
 for filename in glob.glob('../dataset/anti_drone/*.jpg'):
     img = cv2.resize(cv2.imread(filename,0),(a,a)).reshape(1,a**2)[0]
-    antis = np.vstack((antis,img))  
+    antis = np.vstack((antis,img))
 antis = antis[1:]
 
 x = np.vstack((drones,antis))
@@ -29,21 +32,23 @@ train_x = scaler.transform(train_x)
 scaler = preprocessing.StandardScaler().fit(test_x)
 test_x = scaler.transform(test_x)
 
-def cost_function(x, y, model):
-    m = x.shape[0]
-    predict = model.predict(x)
-    total_cost = -(1 / m) * np.sum(y*np.log() + (1 - y) * np.log(
-    1 - hypothesis(theta, x)))
-    return total_cost
-    
-    
-for i in range(10,200,10):
-    model = MLPClassifier(hidden_layer_sizes=2, max_iter=i)
-    model.fit(train_x, train_y)
-    print(model.get_params())
-    cost_train = 1/(2*len(train_y)) * sum(list(map(lambda x:x**2, model.predict(train_x) - train_y)), 0)
-    cost_test = 1/(2*len(test_y)) * sum(list(map(lambda x:x**2, model.predict(test_x) - test_y)), 0)
-    print(cost_train)
-    
-    
-    
+
+model = MLPClassifier(hidden_layer_sizes=2, max_iter=200)
+model.fit(train_x, train_y)
+cv = ShuffleSplit(n_splits=100, test_size=0.2, random_state=0)
+print(cv)
+train_sizes = np.linspace(0.1, 1, 10)
+train_sizes, train_scores, validation_scores = learning_curve(MLPClassifier(), x, y, cv=cv, n_jobs=12,  train_sizes=train_sizes)
+print(train_sizes)
+train_scores_mean = train_scores.mean(axis = 1)
+validation_scores_mean = validation_scores.mean(axis = 1)
+
+plt.plot(train_sizes, 1 - train_scores_mean, label = 'Training error')
+plt.plot(train_sizes, 1 - validation_scores_mean, label = 'Validation error')
+plt.legend()
+#plt.ylim(0,1)
+plt.show()
+
+
+
+
